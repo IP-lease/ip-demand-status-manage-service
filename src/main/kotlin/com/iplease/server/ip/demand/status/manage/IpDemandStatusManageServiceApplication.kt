f2e0f8d31bb6b0c3408881b8d37f51b0a1db47f7
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-22 17:06:31 +0900

    Initial commit

---
+++ src/main/kotlin/com/iplease/server/ip/demand/status/manage/IpDemandStatusManageServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.iplease.server.ip.demand.status.manage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IpDemandStatusManageServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<IpDemandStatusManageServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/iplease/server/ip/demand/status/manage/IpDemandStatusManageServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iplease.server.ip.demand.status.manage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IpDemandStatusManageServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
