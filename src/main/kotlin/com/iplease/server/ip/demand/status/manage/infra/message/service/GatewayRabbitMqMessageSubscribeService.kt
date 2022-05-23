2f96f544201d5eb9abc19e5cb5da55d72c8704b2
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-23 21:14:07 +0900

    [ADD] Message관련Service 구현체 작성 및 이에 필요한 인터페이스 정의

---
+++ src/main/kotlin/com/iplease/server/ip/demand/status/manage/infra/message/service/GatewayRabbitMqMessageSubscribeService.kt
@@ -0,0 +1,28 @@
+package com.iplease.server.ip.demand.status.manage.infra.message.service
+
+import com.iplease.server.ip.demand.status.manage.infra.message.listener.MessageListener
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+
+@Service
+class GatewayRabbitMqMessageSubscribeService: MessageSubscribeService {
+    private val list = mutableSetOf<MessageListener>()
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    @RabbitListener(queues = ["server.ip.demand.status.manage"])
+    fun subscribe(message: Message) {
+        try { subscribe(message.messageProperties.receivedRoutingKey, String(message.body)) }
+        catch (ex: Throwable) { logger.error(ex.message) } //RabbitMQ 메세지 구독중 발생한 모든 오류는 로깅이후 무시한다. (무한loop 방지)
+    }
+
+    override fun subscribe(routingKey: String, message: String) {
+        Flux.fromIterable(list)
+            .map { it.subscribe(routingKey = routingKey, message = message) }
+            .onErrorContinue{ ex, _ -> logger.error(ex.message) } //RabbitMQ 메세지 구독중 발생한 모든 오류는 로깅이후 무시한다. (무한loop 방지)
+            .subscribe()
+    }
+    fun addListener(listener: MessageListener) { list.add(listener) }
+}
---
+++ src/main/kotlin/com/iplease/server/ip/demand/status/manage/infra/message/service/RabbitMqMessagePublishService.kt
@@ -0,0 +1,19 @@
+package com.iplease.server.ip.demand.status.manage.infra.message.service
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class RabbitMqMessagePublishService(
+    val rabbitTemplate: RabbitTemplate
+): MessagePublishService {
+    companion object { const val EXCHANGE_NAME = "iplease.event" }
+    override fun <T: Any>  publish(routingKey: String, data: T) {
+        jacksonObjectMapper().registerModule(JavaTimeModule())
+            .writeValueAsString(data)
+            .let { rabbitTemplate.convertAndSend(EXCHANGE_NAME, routingKey, it) }
+            .let { data }
+    }
+}
