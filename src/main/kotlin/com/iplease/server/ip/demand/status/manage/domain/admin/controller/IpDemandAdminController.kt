633007260bb92c913c3808d530eb6d92be404d11
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-23 15:13:55 +0900

    [FIX] 변경된 스펙에 따라 IpDemandAdminController 수정

--- src/main/kotlin/com/iplease/server/ip/demand/status/manage/domain/admin/controller/IpDemandAdminController.kt
+++ src/main/kotlin/com/iplease/server/ip/demand/status/manage/domain/admin/controller/IpDemandAdminController.kt
@@ -5,25 +5,25 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/v1/ip/demand/admin")
+@RestController
+@RequestMapping("/api/v1/ip/demand/admin")
 class IpDemandAdminController(
     private val ipDemandAdminService: IpDemandAdminService
 ) {
     //IP 할당신청 수락
+    //IP할당 신청성공 Message 발행
     @PostMapping("/{demandUuid}")
-    fun accept(@PathVariable demandUuid: Long): ResponseEntity<Unit> {
-        //IP할당 신청성공 Message 발행
+    fun accept(@PathVariable demandUuid: Long) =
         ipDemandAdminService.accept(demandUuid)
-        return ResponseEntity.ok().build()
-    }
+            .map { ResponseEntity.ok(Unit) }
 
     //IP 할당신청 거절
+    //IP할당 신청실패 Message 발행
     @DeleteMapping("/{demandUuid}")
-    fun reject(@PathVariable demandUuid: Long): ResponseEntity<Unit> {
-        //IP할당 신청실패 Message 발행
+    fun reject(@PathVariable demandUuid: Long) =
         ipDemandAdminService.reject(demandUuid)
-        return ResponseEntity.ok().build()
-    }
+            .map { ResponseEntity.ok(Unit) }
 }
