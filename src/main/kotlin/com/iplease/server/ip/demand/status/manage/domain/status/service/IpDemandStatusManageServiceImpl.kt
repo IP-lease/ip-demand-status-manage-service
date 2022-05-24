f2f4537a04398ddc91edcf7dc3bddfe663266494
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-25 08:25:18 +0900

    [ADD] IpDemandStatusManageServiceImpl에 새로 추가된 상태변경 정책 반영

--- src/main/kotlin/com/iplease/server/ip/demand/status/manage/domain/status/service/IpDemandStatusManageServiceImpl.kt
+++ src/main/kotlin/com/iplease/server/ip/demand/status/manage/domain/status/service/IpDemandStatusManageServiceImpl.kt
@@ -6,6 +6,7 @@ import com.iplease.server.ip.demand.status.manage.global.status.data.type.Demand
 import com.iplease.server.ip.demand.status.manage.global.status.repository.IpDemandStatusRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @Service
 class IpDemandStatusManageServiceImpl(
@@ -16,9 +17,19 @@ class IpDemandStatusManageServiceImpl(
 
     private fun changeDemandStatusByUuid(demandUuid: Long, status: DemandStatusType) =
         ipDemandStatusRepository.findByDemandUuid(demandUuid)
+            .flatMap { checkExists(it) }
+            .flatMap { checkStatus(it, status) }
             .map { it.copy(status = status) }
             .flatMap { ipDemandStatusRepository.save(it) }
             .map { it.toDto() }
+
+    private fun checkExists(table: IpDemandStatusTable?): Mono<IpDemandStatusTable> =
+        table.let { it?.toMono() ?:Mono.error(IllegalArgumentException("demand status table is null")) }
+
+    private fun checkStatus(table: IpDemandStatusTable, tobe: DemandStatusType): Mono<IpDemandStatusTable> =
+            table.toMono()
+                .flatMap { it.status.canChangeTo(tobe) }
+                .map { table } //이미 line26에서 null checking 을 완료하였다.
 
     private fun IpDemandStatusTable.toDto() = IpDemandStatusDto(
         uuid = uuid,
