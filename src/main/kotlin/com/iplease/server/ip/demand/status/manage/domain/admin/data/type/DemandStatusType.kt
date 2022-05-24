e86ca42c6fd19ec7b97c49918b8f5a06979982ce
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-24 14:36:39 +0900

    [FIX] DemandStatusType수정 및 주석추가

--- src/main/kotlin/com/iplease/server/ip/demand/status/manage/domain/admin/data/type/DemandStatusType.kt
+++ src/main/kotlin/com/iplease/server/ip/demand/status/manage/domain/admin/data/type/DemandStatusType.kt
@@ -1,5 +1,10 @@
 package com.iplease.server.ip.demand.status.manage.domain.admin.data.type
 
 enum class DemandStatusType {
-    CREATE, IN_PROGRESS, SUCCESS, REJECT
+    //상태가 처음 등록되었을 경우 CREATE
+    //관리자가 확인하였을 경우 IN_PROGRESS
+    //관리자가 거절하였을 경우 REJECT
+    //관리자가 수락하였을 경우 ACCEPT
+    //IP할당이 완료되었을 경우 SUCCESS
+    CREATE, IN_PROGRESS, REJECT, ACCEPT, SUCCESS
 }
